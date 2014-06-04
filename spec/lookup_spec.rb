40d21885e5db8878fa3e073d02c43bb06cc2e55a
Philip Hale <philip@pghale.com>
2014-06-04 11:46:09 +0100

    Fix indentation of lookup_spec

--- spec/lookup_spec.rb
+++ spec/lookup_spec.rb
@@ -1,13 +1,13 @@
 require 'spec_helper'
 
 describe BrontoGem do
-  
-	context 'when a word is entered' do
-	 	before do
-	 		@response = BrontoGem.lookup("fish")
-	 	end
 
-		it 'returns a hash of synoyms grouped by part of speech' do
+  context 'when a word is entered' do
+    before do
+      @response = BrontoGem.lookup("fish")
+    end
+
+    it 'returns a hash of synonyms grouped by part of speech' do
       fish_response = {
         verb: {
           syn: ["angle", "catch", "grab", "look for", "search", "seek", "take hold of"]
@@ -15,22 +15,22 @@ describe BrontoGem do
         noun: {
           syn: [
             "aquatic vertebrate",
-           "food",
-           "individual",
-           "mortal",
-           "person",
-           "pisces",
-           "pisces the fishes",
-           "solid food",
-           "somebody",
-           "someone",
-           "soul"]
+            "food",
+            "individual",
+            "mortal",
+            "person",
+            "pisces",
+            "pisces the fishes",
+            "solid food",
+            "somebody",
+            "someone",
+            "soul"]
         }
       }
-      
-      expect(@response).to eq fish_response
-		end
 
-	end
-  
+      expect(@response).to eq fish_response
+    end
+
+  end
+
 end
